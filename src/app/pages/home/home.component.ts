56c27ef342c21961a5d205919bbef0ee97185911
Melany Ventura <melanymarisolventura@gmail.com>
2024-10-31 01:06:46 -0400

    Agregue las funcionalidades para el input, como que no acepte null, tambien que cambie el numero de contador de tareas

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,12 @@
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { Task } from './../../models/task.model';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, NgIf, ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -22,11 +24,25 @@ export class HomeComponent {
       completed: false
     },
   ]);
+
+  newTaskCtrl = new FormControl('', {
+    // permite que el input al ingresar la tarea no sea nulo
+    nonNullable: true,
+    validators: [
+      Validators.required,
+    ]
+  });
   // funcion para el input
-  changeHandler(event: Event){
-    const input = event.target as HTMLInputElement;
-    const newTask = input.value;
-    this.addTask(newTask);
+  changeHandler(){
+    if (this.newTaskCtrl.valid){
+      // usamos trim para quitar los espacios de inicio y final
+      const value = this.newTaskCtrl.value.trim();
+      if (value !== ''){
+        this.addTask(value);
+        //una vez agregada la tarea el input queda vacio
+        this.newTaskCtrl.setValue('');
+      }  
+    }
   }
   // funcion para agregar una tarea
   addTask(title: string){
@@ -40,6 +56,53 @@ export class HomeComponent {
   // funcion para eliminar una tarea
   deleteTask(index: number){
     this.tasks.update((tasks) => tasks.filter((task, position) => position !== index));
+  }
+  // funcion para actualizacion de la tarjeta
+  updateTask(index: number){
+    this.tasks.update((tasks) => {
+      return tasks.map((task, position) => {
+        if (position === index){
+          return{
+            ...task,
+            completed: !task.completed
+          }
+        }
+        return task;
+      })
+    })
+  }
+  // funcion para editar la tarea
+  updateTaskEditingMode(index:number){
+    this.tasks.update(prevState => {
+      return prevState.map((task, position) => {
+        if (position === index){
+          return {
+            ...task,
+            editing: true,
+          }
+        }
+        return {
+          ...task,
+          editing: false,
+        }
+      });
+    })
+  }
+  // funcion para la tarea editada actualizada
+  updateTaskText(index:number, event: Event){
+    const input = event.target as HTMLInputElement;
+    this.tasks.update(prevState => {
+      return prevState.map((task, position) => {
+        if (position === index){
+          return {
+            ...task,
+            title: input.value,
+            editing: false,
+          }
+        }
+        return task;
+      });
+    })
   }
 }
 
